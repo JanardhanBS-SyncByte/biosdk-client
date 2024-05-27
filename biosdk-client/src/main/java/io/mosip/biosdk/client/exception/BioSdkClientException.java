671d7bf94ad6d343c47cadc952a4bc2b7cd3ba46
JanardhanBS-SyncByte <janardhan@syncbyte.in>
2024-05-27 12:43:03 +0530

    [MOSIP-33324]
    Signed-off-by: JanardhanBS-SyncByte <janardhan@syncbyte.in>

--- biosdk-client/src/main/java/io/mosip/biosdk/client/exception/BioSdkClientException.java
+++ biosdk-client/src/main/java/io/mosip/biosdk/client/exception/BioSdkClientException.java
@@ -10,25 +10,15 @@ import io.mosip.kernel.core.exception.BaseUncheckedException;
  * @since 1.0.0
  */
 public class BioSdkClientException extends BaseUncheckedException {
-
 	/**
-	 * Constructs a BioSdkClientException with an error code and message.
-	 *
-	 * @param errorCode    The error code associated with the exception.
-	 * @param errorMessage The error message describing the exception.
+	 * 
 	 */
 	public BioSdkClientException(String errorCode, String errorMessage) {
 		super(errorCode, errorMessage);
 	}
 
 	/**
-	 * Constructs a BioSdkClientException with an error code, message, and root
-	 * cause exception.
-	 *
-	 * @param errorCode    The error code associated with the exception.
-	 * @param errorMessage The error message describing the exception.
-	 * @param rootCause    The underlying cause (throwable) that led to this
-	 *                     exception.
+	 * 
 	 */
 	public BioSdkClientException(String errorCode, String errorMessage, Throwable rootCause) {
 		super(errorCode, errorMessage, rootCause);
